1c49eb5a85f82a4f5e0a7ac29a67e7c8c4721efd
Zero21 Ops01 <ops01@zero21.io>
2022-02-24 21:24:44 +0200

    .

--- be/core/classes/MysqlDataLayer.ts
+++ be/core/classes/MysqlDataLayer.ts
@@ -5,7 +5,7 @@ export class MysqlDataLayer {
     async test() {
         const con = await mysql.createConnection({host:'localhost', user: 'nuxt01', database: 'nuxt01', password: '123qwe123'})
         promisify(con.query).bind(con)
-        const [rows, fields] = await con.execute('SELECT * FROM `test`;')
-        return {rows, fields}
+        const [rows] = await con.execute('SELECT * FROM `test`;')
+        return { rows }
     }
 }
